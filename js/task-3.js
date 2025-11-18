edd403b5e17e620c2d766af2ac0757410c636256
Banana <x.boogieboo.x@gmail.com>
2025-11-19 00:01:09 +0200

    delete console.log in function

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,7 @@
 function getElementWidth(content, padding, border) {
-    console.log(Number.parseInt(content) + Number.parseInt(padding) * 2 + Number.parseFloat(border) * 2)
+    return(Number.parseInt(content) + Number.parseInt(padding) * 2 + Number.parseFloat(border) * 2)
 }
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
